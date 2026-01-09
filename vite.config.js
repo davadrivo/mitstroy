c839eb048bdc93765f104f864ad6d20e36fef5dd
davadrivo <davadrivo@gmail.com>
2026-01-09 19:20:28 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/mit-stroy/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/mitstroy/' : '/',
 })
-
 
 
